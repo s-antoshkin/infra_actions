7b73e84cedd0ee06a32116dafb755b55b4ee5b69
Semyon Antoshkin <s.s.antoshkin@gmail.com>
2022-04-11 20:50:28 +0500

    update workflow main.yml

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Рот наоборот того!')
 
 
 def second_page(request):
